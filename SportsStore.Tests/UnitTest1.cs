861c24ccd7578ddc32275347e0c0a63c9c573f59
Victor Fonseca <victorjvaf2012@gmail.com>
2021-01-05 21:30:47 -0300

    xunit

---
+++ SportsStore.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using AppSportsStore.Models;
+
+namespace SportsStore.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
